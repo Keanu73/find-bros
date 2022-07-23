710e224c1a9ff392a211194c96eab79ccb932cb8
retaps <retaps@null>
2022-07-23 18:29:04 +0200

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,37 @@
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Find self improvement servers</title>
+
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <meta property="og:title" content="Find your bros" />
+        <meta
+          property="og:description"
+          content="Find self improvement communities from your country"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: 'light',
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,95 @@
+import type { NextPage } from 'next';
+import { Button, Select, Transition, Modal, List, Text } from '@mantine/core';
+import Head from 'next/head';
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+import servers from 'servers.json';
+import { MutableRefObject, useRef, useState } from 'react';
+
+const Home: NextPage = () => {
+  const [openedModal, setOpenedModal] = useState(false);
+  const [country, setCountry] = useState<string | null>('');
+  const ref = useRef<HTMLInputElement>() as MutableRefObject<HTMLInputElement>;
+
+  return (
+    <div className={styles.container}>
+      <Modal
+        centered
+        opened={openedModal}
+        onClose={() => setOpenedModal(false)}
+        title="Country not found!"
+        size={'lg'}
+      >
+        <Text>
+          {
+            "Sadly we don't have servers for every country as of now, but we would greatly appreciate it if you created one for yours."
+          }
+          <br />
+          <br />
+          {"Here's the steps you can follow if you choose to do so:"}
+        </Text>
+
+        <List mt={'xl'}>
+          <List.Item>
+            Create a server using the following{' '}
+            <a href="https://discord.com/template/2AAUGBqBm3Nm">template</a>.
+          </List.Item>
+          <List.Item>
+            Contact <b>Agro7#0007</b> on Discord so he can add your server to
+            the list.
+          </List.Item>
+        </List>
+      </Modal>
+
+      <main className={styles.main}>
+        <Select
+          className={styles.search}
+          ref={ref}
+          searchable
+          clearable
+          onChange={setCountry}
+          label="Type your country here"
+          data={servers.map((s) => s.country)}
+        />
+
+        <Transition
+          mounted={!!country?.length}
+          transition={'skew-up'}
+          timingFunction={'ease'}
+          duration={200}
+        >
+          {(styles) => (
+            <Link
+              href={servers.find((s) => s.country === country)?.invite || 'tmp'}
+            >
+              <Button style={styles} variant={'light'}>
+                Join server for {country}
+              </Button>
+            </Link>
+          )}
+        </Transition>
+      </main>
+
+      <footer className={styles.footer}>
+        <Button
+          color={'red'}
+          variant="outline"
+          onClick={() => setOpenedModal(true)}
+        >
+          {"Can't find your country?"}
+        </Button>
+
+        <Text
+          mt={'md'}
+          sx={(theme) => ({
+            color: theme.colors.gray[5],
+          })}
+        >
+          (c) retaps, 2022 // felixsx#9501
+        </Text>
+      </footer>
+    </div>
+  );
+};
+
+export default Home;
