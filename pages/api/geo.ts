99e426a97d536c0705774c4fc4f90c0a05699037
retaps <retaps@null>
2022-07-23 19:02:31 +0200

    e

--- pages/api/geo.ts
+++ pages/api/geo.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import cc from 'country-codes.json';
 
 type Data = {
   country: string;
@@ -10,9 +11,12 @@ export default function handler(
   res: NextApiResponse<Data>
 ) {
   console.log(req.headers);
-  res
-    .status(200)
-    .json({
-      country: (req.headers['x-vercel-ip-country'] as string) || 'unknown',
-    });
+
+  const country =
+    cc.find((c) => c.code === (req.headers['x-vercel-ip-country'] as string))
+      ?.name || 'United States';
+
+  res.status(200).json({
+    country,
+  });
 }
