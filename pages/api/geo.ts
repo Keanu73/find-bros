a7a77dc4948a3ded1a8aaeb904031efeabecae4f
retaps <retaps@null>
2022-07-23 18:58:19 +0200

    add geo

---
+++ pages/api/geo.ts
@@ -0,0 +1,18 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+type Data = {
+  country: string;
+};
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  console.log(req.headers);
+  res
+    .status(200)
+    .json({
+      country: (req.headers['x-vercel-ip-country'] as string) || 'unknown',
+    });
+}
